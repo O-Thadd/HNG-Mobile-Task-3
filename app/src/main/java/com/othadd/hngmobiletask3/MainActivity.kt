0c8e268f286d29267040c0350895918bc5c1f842
thadd <thaddeusojike@gmail.com>
2022-11-10 03:15:28 +0100

    added navigation component

--- app/src/main/java/com/othadd/hngmobiletask3/MainActivity.kt
+++ app/src/main/java/com/othadd/hngmobiletask3/MainActivity.kt
@@ -2,10 +2,22 @@ package com.othadd.hngmobiletask3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentContainerView =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = fragmentContainerView.navController
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
